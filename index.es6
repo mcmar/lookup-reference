aafdfd75344552c5cf30ad9653e2dc184423adba
Peter Moresi <peter.moresi@gmail.com>
2015-09-18 00:16:30 -0700

    first build

--- index.es6
+++ index.es6
@@ -1,13 +1,15 @@
 export default {
-  ADDRESS: throw new Error('Not implemented.'),
-  COLUMN: throw new Error('Not implemented.'),
-  COLUMNS: throw new Error('Not implemented.'),
+  ADDRESS: new Error('Not implemented.'),
+  COLUMN: new Error('Not implemented.'),
+  COLUMNS: new Error('Not implemented.'),
   LOOKUP: require('formula-lookup'),
   HLOOKUP: require('formula-hlookup'),
   INDEX: require('formula-vlookup'),
   INDIRECT: require('formula-vlookup'),
-  OFFSET: throw new Error('Not implemented.'),
-  MATCH: throw new Error('Not implemented.'),
+  MATCH: new Error('Not implemented.'),
+  OFFSET: new Error('Not implemented.'),
+  ROW: new Error('Not implemented.'),
+  ROWS: new Error('Not implemented.'),
   VLOOKUP: require('formula-vlookup'),
 
 }
---
+++ index.js
@@ -0,0 +1,31 @@
+(function (global, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['exports', 'module'], factory);
+  } else if (typeof exports !== 'undefined' && typeof module !== 'undefined') {
+    factory(exports, module);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports, mod);
+    global.index = mod.exports;
+  }
+})(this, function (exports, module) {
+  'use strict';
+
+  module.exports = {
+    ADDRESS: new Error('Not implemented.'),
+    COLUMN: new Error('Not implemented.'),
+    COLUMNS: new Error('Not implemented.'),
+    LOOKUP: require('formula-lookup'),
+    HLOOKUP: require('formula-hlookup'),
+    INDEX: require('formula-vlookup'),
+    INDIRECT: require('formula-vlookup'),
+    MATCH: new Error('Not implemented.'),
+    OFFSET: new Error('Not implemented.'),
+    ROW: new Error('Not implemented.'),
+    ROWS: new Error('Not implemented.'),
+    VLOOKUP: require('formula-vlookup')
+
+  };
+});
